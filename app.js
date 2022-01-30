f4e0d789d363136ea19a71696acc53f30d791c6e
marah-jaradat <marahjaradat97@gmail.com>
2022-01-30 07:30:00 +0200

    adding requirments for task 08

--- app.js
+++ app.js
@@ -6,27 +6,74 @@ let department = ["administration", "marketing", "development", "finance"];
 let level = ["Junior", "Mid-Senior", "Senior"];
 let allEmployees = [];
 
+// Define:
+
+let headerParent = document.getElementById("headerParent");
+// let title = document.createElement("h2");
+// title.textContent = "employeeee";
+// headerParent.appendChild(title);
+
+let myEmployDiv = document.getElementById("myEmploy");
+let main = document.getElementById("main");
+let myForm = document.getElementById("form");
+let myBtn = document.getElementById("btn");
+
 // My Constructor:
 
-function Employee(employeeID, fullName, department, level) {
-  this.employeeID = employeeID;
+function Employee(fullName, department, level, img) {
+  this.employeeID = 0;
   this.fullName = fullName;
   this.department = department;
   this.level = level;
-  //   this.img = img;
+  this.img = img;
   this.salary = 0;
 
   allEmployees.push(this);
 }
 
 // Instances:
-let ghazi = new Employee(1000, "Ghazi Samer", department[0], level[2]);
-let lana = new Employee(1001, "Lana Ali", department[3], level[2]);
-let tamara = new Employee(1002, "Tamara Ayoub	", department[1], level[2]);
-let safi = new Employee(1003, "Safi Walid	", department[0], level[1]);
-let omar = new Employee(1004, "Omar Zaid	", department[2], level[2]);
-let rana = new Employee(1005, "Rana Saleh	", department[2], level[0]);
-let hadi = new Employee(1006, "Hadi Ahmad	", department[3], level[2]);
+let ghazi = new Employee(
+  "Ghazi Samer",
+  department[0],
+  level[2],
+  "/assets/Ghazi.jpg"
+);
+let lana = new Employee(
+  "Lana Ali",
+  department[3],
+  level[2],
+  "/assets/Lana.jpg"
+);
+let tamara = new Employee(
+  "Tamara Ayoub	",
+  department[1],
+  level[2],
+  "/assets/Tamara.jpg"
+);
+let safi = new Employee(
+  "Safi Walid	",
+  department[0],
+  level[1],
+  "/assets/Safi.jpg"
+);
+let omar = new Employee(
+  "Omar Zaid	",
+  department[2],
+  level[2],
+  "/assets/Omar.jpg"
+);
+let rana = new Employee(
+  "Rana Saleh	",
+  department[2],
+  level[0],
+  "/assets/Rana.jpg"
+);
+let hadi = new Employee(
+  "Hadi Ahmad	",
+  department[3],
+  level[2],
+  "/assets/Hadi.jpg"
+);
 
 // Functions:
 
@@ -41,36 +88,57 @@ Employee.prototype.randomSalary = function (min, max) {
   this.salary = ran - ran * 0.075;
 };
 
-// check to assign the salary
+Employee.prototype.randomID = function (min, max) {
+  this.employeeID = getRndInteger(min, max);
+};
 
+Employee.prototype.render = function () {
+  let empName = document.createElement("h5");
+  empName.textContent = this.fullName;
+  myEmployDiv.appendChild(empName);
+  let imgg = document.createElement("img");
+  imgg.setAttribute("src", this.img);
+  myEmployDiv.appendChild(imgg);
+  imgg.style.width = "200px";
+};
+
+for (let i = 0; i < allEmployees.length; i++) {
+  allEmployees[i].randomID(1000, 9999);
+  // console.log(allEmployees[i]);
+  allEmployees[i].render();
+}
+
+// check to assign the salary
+// function renderAll() {
 for (let i = 0; i < allEmployees.length; i++) {
   if (allEmployees[i].level === "Junior") {
     allEmployees[i].randomSalary(500, 1000);
-    console.log(allEmployees[i]);
+    // console.log(allEmployees[i]);
   } else if (allEmployees[i].level === "Mid-Senior") {
     allEmployees[i].randomSalary(1000, 1500);
-    console.log(allEmployees[i]);
+    // console.log(allEmployees[i]);
   } else if (allEmployees[i].level === "Senior") {
     allEmployees[i].randomSalary(1500, 2000);
-    console.log(allEmployees[i]);
   }
+  // console.log(allEmployees[i]);
 }
-// Render on Screen:
+// }
+// renderAll();
 
-Employee.prototype.render = function () {
-  document.write("Name:" + `${this.fullName}`);
-  document.write("Salary :" + `${this.salary}`);
-};
+// Adding Events:
+myForm.addEventListener("submit", createSubmit);
+function createSubmit(event) {
+  event.preventDefault();
+  // console.log(event);
+  let name = event.target.employeeName.value;
+  // let dep = event.target.value;
+  // let lev = event.target.levForm.value;
+  let images = event.target.images.value;
 
-ghazi.render();
-lana.render();
-tamara.render();
-safi.render();
-omar.render();
-rana.render();
-hadi.render();
-
-// console.log(getRndInteger(500, 1000));
-// console.log(getRndInteger(1000, 1500));
-// console.log(getRndInteger(1500, 2000));
-// // console.log(allEmployees);
+  let newEmployee = new Employee(name, images);
+  newEmployee.randomSalary();
+  newEmployee.randomID();
+  // renderAll();
+  newEmployee.render();
+  // console.log(dep);
+}
